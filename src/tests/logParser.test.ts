cf3ad5e261fbfbcbde846d2ee24c1ee79de21dc9
shubhamdikshitofficial <shubhamdixit912@gmail.com>
2024-01-12 16:06:57 +0530

    code for app added

--- src/tests/logParser.test.ts
+++ src/tests/logParser.test.ts
@@ -1,31 +1,9 @@
-import { LogParser } from '../parser';
 import * as fs from 'fs';
 import { spawnSync } from 'child_process';
-// describe('LogParser', () => {
-//   it('should parse logs and return errors', () => {
-//     // Arrange
-//     const logParser = new LogParser();
-//     const sampleLogs = [
-//       '2022-01-15T10:30:45.123Z - info - {"transactionId":"1234","details":"Service is started"}',
-//       '2022-01-15T10:31:15.456Z - error - {"transactionId":"5678","details":"Error occurred","err":"Something went wrong"}',
-//       // Add more sample logs as needed
-//     ];
-
-//     // Act
-//     const errors = logParser.parseLogs(sampleLogs);
-
-//     // Assert
-//     expect(errors).toHaveLength(1);
-//     expect(errors[0].loglevel).toBe('error');
-//     expect(errors[0].transactionId).toBe('5678');
-//     expect(errors[0].err).toBe('Something went wrong');
-//     // Add more assertions as needed
-//   });
-// });;
 
 describe('Integration Test', () => {
   it('should parse logs and generate errors.json', () => {
-    // Arrange
+
     const inputFilePath = './app.log';
     const outputFilePath = './errors.json';
     const expectedResult = [
@@ -34,22 +12,17 @@ describe('Integration Test', () => {
         loglevel: 'error',
         transactionId: '9abc55b2-807b-4361-9dbe-aa88b1b2e978',
         err: 'Not found',
-      },
-      // Add more expected results as needed
+      }
     ];
 
-    // Act
-    const command = 'node';  // Assuming the Node.js executable is in your PATH
+    const command = 'node';  
     const args = ['parser.js', '--input', inputFilePath, '--output', outputFilePath];
     const result = spawnSync(command, args);
 
-    // Assert
     expect(result.error).toBeUndefined();
     expect(result.status).toBe(0);
 
-    // Verify the content of errors.json
     const actualResult = JSON.parse(fs.readFileSync(outputFilePath, 'utf-8'));
     expect(actualResult).toEqual(expectedResult);
-    // Add more assertions as needed
   });
 });
